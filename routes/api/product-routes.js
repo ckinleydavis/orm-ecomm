b9c4b6fe9b91e3f426f03a3b53fa779826e6ba0d
Kinley Davis <d8tascientist@gmail.com>
2021-08-15 15:03:09 -0400

    continue work on product queries

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,7 +26,7 @@ router.get('/:id', async( req, res ) => {
 });
 
 // Create new product
-router.post('/', (req, res) => {
+router.post('/', ( req, res ) => {
     Product.create(req.body)
         .then((product) => {
             /* if there's product tags, we need to create pairings to 
@@ -47,6 +47,54 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(400).json(err)
         });
+});
+
+// Update product
+router.put('/:id', ( req, res ) => {
+    // Update product data
+    Product.update( req.body, {
+        where: {
+            id: req.params.id,
+        },
+    })
+    .then((product) => {
+        // Find all associated tags from ProductTag
+        return ProductTag.findAll({
+            where: {
+                product_id: req.params.id
+            }
+        });
+    })
+    .then((productTags) => {
+        // Get list of current tag_ids
+        const productTagIds = productTags.map(({ tag_id }) => tag_id );
+        // Create filtered list of new tag_ids
+        const newProductTags = req.body.tagIds
+            .filter((tag_id) => !productTagIds.includes(tag_id))
+            .map((tag_id) => {
+                return {
+                    product_id: req.params.id, tag_id
+                };
+            });
+        // Figure out which ones to remove
+        const productTagsToRemove = productTags
+            .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+            .map(({ id }) => id);
+        // Run both actions
+        return Promise.all([
+            ProductTag.destroy({ where: { id: productTagsToRemove } }),
+            ProductTag.bulkCreate(newProductTags)
+        ]);
+    })
+    .then((updatedProductTags) => res.json(updatedProductTags))
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err); 
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    // Delete one product by its `id` value
 });
 
 module.exports = router;
