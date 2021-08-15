a6c627a4d9428ed87470746ab6fad29e0c2cd3b7
Kinley Davis <d8tascientist@gmail.com>
2021-08-15 10:32:58 -0400

    added table relationships

---
+++ models/index.js
@@ -0,0 +1,33 @@
+// Import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Product belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE'
+});
+
+// Products belongToMany Tags (thru ProductTag)
+Product.hasMany(ProductTag, {
+    foreignKey: 'tag_id'
+});
+
+// Tags belongToMany Products (thru ProductTag)
+Tag.hasMany(ProductTag, {
+    foreignKey: 'product_id'
+});
+
+module.exports = {
+    Product,
+    Category,
+    Tag,
+    ProductTag
+};
