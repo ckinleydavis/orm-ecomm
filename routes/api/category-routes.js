8661f394839dbbec1199806f4e9f683a27662b7f
Kinley Davis <d8tascientist@gmail.com>
2021-10-11 00:51:34 -0400

    update code to category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,26 +10,68 @@ router.get("/", async (req, res) => {
                 attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
             },
         });
-
         res.status(200).json(categoryData);
     } catch (err) {
-        res.status(500).json(err)
+        res.status(500).json(err);
     }
 });
 
 // Create New Category
 router.post("/", async (req, res) => {
     try {
-
+        const categoryData = await Category.create({
+            category_name: req.body.category_name
+        });
+        res.status(200).json(categoryData);
     } catch (err) {
-
+        res.status(400).json(err);
     }
 });
 
 // Find Category and its Products by ID
+router.get("/:id", async (req, res) => {
+    try {
+        const categoryData = await Category.findOne ({
+            where: { id: req.params.id },
+            include: {
+                model: Product,
+                attributes: [ 'id', 'product_name', 'price', 'stock', 'category_id' ]
+            }
+        })
+    res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // Update Category
+router.get("/:id", async (req, res) => {
+    try {
+        const categoryData = await Category.update(
+            { category_name: req.body.category_name },
+            { where: { id: req.params.id } },
+        );  
+    res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // Delete Category
+router.delete("/:id", async (req, res) => {
+    try {
+        const categoryData = await Category.destroy({
+            where: { id: req.params.id }
+        });
+        
+        if (!categoryData) {
+            res.status(404).json({ message: "Category could not be found!" });
+            return;
+        }
+    res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
