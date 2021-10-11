65f4fa89db3f7bd8b7b77732a7019b57aa13a496
Kinley Davis <d8tascientist@gmail.com>
2021-10-11 01:17:58 -0400

    fixed missing async

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -116,17 +116,15 @@ router.put('/:id', ( req, res ) => {
 });
 
 // Delete one product by its `id` value
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     try {
         const productData = await Product.destroy({
             where: { id: req.params.id }
     });
 
     if (!productData) {
-        res.status(404).json(
-            { message: 'No product found with this id!' }
-        );
-    return;
+        res.status(404).json({ message: 'Product could not be found!' });
+        return;
     }
         res.status(200).json(productData);
     } catch (err) {
